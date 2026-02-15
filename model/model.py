86112baacfc9b2b5a6b7ed3f7170d406ce0aa07e
XLeafAutumn <xujunyao1130@gmail.com>
2026-02-16 02:09:07 +0800

    github

--- model/model.py
+++ model/model.py
@@ -94,3 +94,48 @@ class RMSNorm(nn.Module):
     def forward(self, x):
         return self.weight * self._norm(x.float()).typed_as(x)
 
+# ROPE
+def precompute_freqs(
+    dim: int,
+    end: int = int(32 * 1024),
+    rope_base: float = 1e6,
+    rope_scaling: Optional[dict] = None,
+):
+    freqs = 1.0 / (rope_base ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+
+    if rope_scaling is not None:
+        original_max, factor, beta_fast, beta_slow = (
+            rope_scaling.get("original_max_position_embeddings", 2048),
+            rope_scaling.get("factor", 4),
+            rope_scaling.get("beta_fast", 4.0),
+            rope_scaling.get("beta_slow", 1.0),
+        )
+
+        if end / original_max > 1.0:
+            corr_dim = next(
+                (i for i in range(dim // 2) if 2 * math.pi / freqs[i] > original_max),
+                dim // 2,
+            )
+
+            power = torch.arange(0, dim // 2, device=freqs.device).float() / max(
+                dim // 2 - 1, 1
+            )
+
+            beta = beta_slow + (beta_fast - beta_slow) * power
+
+            scale = torch.where(
+                torch.arange(dim // 2, device=freqs.device) < corr_dim,
+                (beta * factor - beta + 1) / (beta * factor),
+                1.0 / factor,
+            )
+
+            freqs = freqs * scale
+
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+
+    freqs_cos = torch.cat([torch.cos(freqs), torch.cos(freqs)], dim=-1)
+    freqs_sin = torch.cat([torch.sin(freqs), torch.sin(freqs)], dim=-1)
+
+    return freqs_cos, freqs_sin
+
